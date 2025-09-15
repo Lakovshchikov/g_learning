c01333304f4b612a43f861fd6d7e8583a2954559
Ivan V Lakovschikov <lakovshikov@gmail.com>
2025-09-15 19:18:51 +0300

    init

---
+++ src/main/java/org/yourcompany/g_learning/G_Learning.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.g_learning;
+
+/**
+ *
+ * @author lakov
+ */
+public class G_Learning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/org/yourcompany/g_learning/lesson1/lesson1.java
@@ -0,0 +1,13 @@
+
+
+package org.yourcompany.g_learning.lesson1;
+
+/**
+ *
+ * @author lakov
+ */
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
